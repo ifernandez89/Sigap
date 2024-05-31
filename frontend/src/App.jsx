05573d562beca70ef7df5f313b699b8f15892862
Ignacio Fernández <nacho.xiphos@gmail.com>
2024-05-31 09:27:38 -0300

    actualizacion de push en repo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import AgregarFactura from './components/agregarfactura'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AgregarFactura/>
     </>
   )
 }
--- frontend/src/components/agregarfactura.jsx
+++ frontend/src/components/agregarfactura.jsx
@@ -1,8 +1,15 @@
-import {Link} from "react-router-dom"
-import React,{useState} from 'react'
+import { Link } from "react-router-dom"
+import React, { useState } from 'react'
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button, Box, Image, Heading
+} from '@chakra-ui/react'
 
 export default function AgregarFactura() {
-    
+
     const [fecha, setFecha] = useState(null);
     const [nfact, setNfact] = useState(null);
     const [nexp, setNexp] = useState("");
@@ -24,92 +31,133 @@ export default function AgregarFactura() {
     const [t4potconv, setT4potconv] = useState("");
     const [consalerta, setConsalerta] = useState("");
 
-    function SetNfact(e){
+    function SetNfact(e) {
         setNfact(e.target.value);
     }
-    function SetFecha(e){
+    function SetFecha(e) {
         setFecha(e.target.value);
     }
-    function SetNexp(e){
+    function SetNexp(e) {
         setNexp(e.target.value);
     }
-    function SetFolio(e){
+    function SetFolio(e) {
         setFolio(e.target.value);
     }
-    function SetImpvenc(e){
+    function SetImpvenc(e) {
         setImpvenc(e.target.value);
     }
-    function SetNis(e){
+    function SetNis(e) {
         setNis(e.target.value);
     }
-    function setCondfiscs(e){
+    function setCondfiscs(e) {
         setCondfisc(e.target.value);
     }
-    function SetConsact(e){
+    function SetConsact(e) {
         setConsact(e.target.value);
     }
-    function SetInfodes(e){
+    function SetInfodes(e) {
         setInfodes(e.target.value);
     }
-    function SetCosphi(e){
+    function SetCosphi(e) {
         setCosphi(e.target.value);
     }
-    function SetFactorpot(e){
+    function SetFactorpot(e) {
         setFactorpot(e.target.value);
     }
-    function SetRecfactpot(e){
+    function SetRecfactpot(e) {
         setRecfactpot(e.target.value);
     }
-    function SetPtapotconv(e){
+    function SetPtapotconv(e) {
         setPtapotconv(e.target.value);
     }
-    function SetPtapotreg(e){
+    function SetPtapotreg(e) {
         setPtapotreg(e.target.value);
     }
-    function SetFptapotconv(e){
+    function SetFptapotconv(e) {
         setFptapotconv(e.target.value);
     }
-    function SetFptapotreg(e){
+    function SetFptapotreg(e) {
         setFptapotreg(e.target.value);
     }
-    function SetFptapotconve(e){
+    function SetFptapotconve(e) {
         setFptapotconve(e.target.value);
     }
-    function SetFptapotregi(e){
+    function SetFptapotregi(e) {
         setFptapotregi(e.target.value);
     }
-    function SetT4potconv(e){
+    function SetT4potconv(e) {
         setT4potconv(e.target.value);
     }
-    function SetConsalerta(e){
+    function SetConsalerta(e) {
         setConsalerta(e.target.value);
     }
-    
+
     function aceptar(e) {
         e.preventDefault();
-            if (nfact != null) {
-                fetch('http://127.0.0.1:8000/api/AgregarAlumno', {
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        Dni: dni,
-                        Nombre: nombre,
-                        Apellido: apellido
-                    })
-                }).then(() => {
-                    setApellido('');
-                    setNombre('');
-                    setDni('');
-                    window.location.reload();
-                }).catch(error => {
-                    console.error('Error en la solicitud:', error.message);
-                    alert('Alumno ya existente');
-                });
-            }
+        if (nfact != null) {
+            fetch('http://127.0.0.1:8000/api/AgregarFactura', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    fecha: fecha,
+                    nfact: nfact,
+                    nexp: nexp,
+                    folio: folio,
+                    importepvenc: importepvenc,
+                    nisnu: nisnu,
+                    nomsec: "",
+                    zona: "",
+                    tarifa: "",
+                    condfisciva: condfisciva,
+                    consactual: consactual,
+                    infodes: infodes,
+                    cosphi: cosphi,
+                    factorpot: factorpot,
+                    recfactpot: recfactpot,
+                    ptapotconv: ptapotconv,
+                    ptapotreg: ptapotreg,
+                    fptapotconv: fptapotconv,
+                    fptapotreg: fptapotreg,
+                    fptapotconve: fptapotconve,
+                    fptapotregi: fptapotregi,
+                    t4potconv: t4potconv
+                })
+            }).then(() => {
+                setFecha('');
+                setNfact('');
+                setNexp('');
+                setFolio('');
+                setImpvenc('');
+                setNis('');
+                setCondfisc('');
+                setConsact('');
+                setInfodes('');
+                setCosphi('');
+                setFactorpot('');
+                setRecfactpot('');
+                setPtapotconv('');
+                setPtapotreg('');
+                setFptapotconv('');
+                setFptapotreg('');
+                setFptapotconve('');
+                setFptapotregi('');
+                setT4potconv('');
+                setConsalerta('');
+                window.location.reload();
+            }).catch(error => {
+                console.error('Error en la solicitud:', error.message);
+                alert('Factura existente');
+            });
+        }
     }
-    return(
-
-    )
+    return (
+        <>
+                <div>
+                    <button>volver a mesas</button>
+                </div>
+       
+        </>
+    );
 }
